484547d6bc9424a3bafd5f960b4342481c08cb09
jini <jini9256@gmail.com>
2022-12-29 16:41:04 +0900

    RN Prac(1

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={{ width: 100, height: 100, backgroundColor: "teal" }}></View>
+      <View
+        style={{ width: 100, height: 100, backgroundColor: "orange" }}
+      ></View>
+      <View
+        style={{ width: 100, height: 100, backgroundColor: "tomato" }}
+      ></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
